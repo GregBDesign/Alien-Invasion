0a43564dbb84806bb53de42700e577d62c66071e
Greg <80447308+GregBDesign@users.noreply.github.com>
2021-07-25 14:05:03 +1000

    Update to functionality

---
+++ game_stats.py
@@ -0,0 +1,14 @@
+class GameStats:
+    """Track stats throughout the game"""
+
+    def __init__(self, ai_game):
+        """Initialise statistics"""
+        self.settings = ai_game.settings
+        self.reset_stats()
+        
+        # Start game in active state
+        self.game_active = True
+
+    def reset_stats(self):
+        """Initialise statistics that can change during gameplay"""
+        self.ships_left = self.settings.ship_limit
--- settings.py
+++ settings.py
@@ -4,15 +4,16 @@ class Settings:
     def __init__(self):
         """Initialise game settings"""
         # Screen settings
-        self.screen_width = 1200
+        self.screen_width = 1000
         self.screen_height = 700
         self.bg_color = (230, 230, 230)
 
         # Ship settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 2
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
@@ -20,3 +21,7 @@ class Settings:
 
         # Alien settings
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 = right, -1 = left
+        self.fleet_direction = 1
+        
